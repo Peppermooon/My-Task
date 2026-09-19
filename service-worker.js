64ca60c37f1c8e32aa44c4e0263892ccb6dbc429
Himal Jay <himaldjayasekara@gmail.com>
2026-09-19 22:05:25 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-tasks-v10-settings-fix';
+const CACHE_NAME = 'my-tasks-v11-viewport-wallpaper';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
